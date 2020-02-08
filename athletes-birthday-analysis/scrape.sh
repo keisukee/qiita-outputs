88552e9548113efa3f48610763e0b70c9c25670a
keisuke ohashi <kog71903@gmail.com>
2020-02-08 23:35:10 +0900

    シェルスクリプト作成

---
+++ athletes-birthday-analysis/scrape.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+ruby scrape2.rb data_basketball_player.txt > output/basketball_player_birthday.txt
+ruby scrape2.rb data_baseball_player.txt > output/soccer_player_birthday.txt
+ruby scrape2.rb data_basketball_player.txt > output/basketball_player_birthday.txt
